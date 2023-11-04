3f61b24cfc9940bd6f2183bafb380763faf41703
Anand-021 <anandkumarmaski088@gmail.com>
2023-11-04 19:41:12 +0530

    updated palindrome program

--- PalindromeExample.java
+++ PalindromeExample.java
@@ -1,3 +1,6 @@
+/*A palindrome number is a number that is same after reverse.
+ For example 545, 151, 34543, 343, 171, 48984 are the palindrome numbers.
+  It can also be a string like LOL, MADAM etc. */
 public class PalindromeExample {
     public static void main(String args[]){  
         int r,sum=0,temp;    
